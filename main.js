adc7ea3aac1f3b71601b4b9ee819066960f6526d
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2022-12-07 20:15:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function take(){
         document.getElementById("result").innerHTML = '<img id="capturedImg" src="'+data_uri+'"/>'
     })
 }
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/0uCvdwLQ6/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/W8M4YWJFJ/model.json",modelLoaded);
 function modelLoaded(){
     console.log("model loaded")
 }
@@ -25,4 +25,22 @@ function speak(){
     data2 = "a segunda previsão é "+predction2
     var utterthis = new SpeechSynthesisUtterance(data1+data2);
     synth.speak(utterthis)
+}
+
+function check(){
+    img = document.getElementById("capturedImg");
+    classifier.classify(img,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }else{
+        console.log(results)
+        document.getElementById("resultEmotionName").innerHTML = results[0].label
+        document.getElementById("resultEmotionName2").innerHTML = results[1].label
+        predction1 = results[0].label
+        predction2 = results[1].label
+        speak()
+    }
 }
